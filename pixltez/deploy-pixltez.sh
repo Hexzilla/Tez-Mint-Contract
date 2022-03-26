bf62b0b1004ba12098e9220a2912cdfd0fd1a316
Eric Thomas <bluebird6922@gmail.com>
2022-03-26 12:17:03 +0300

    update deploy script

--- pixltez/deploy-pixltez.sh
+++ pixltez/deploy-pixltez.sh
@@ -1,1 +1,1 @@
-SmartPy.sh originate-contract --code ./build/PiXLtez/step_000_cont_0_contract.tz --storage ./build/PiXLtez/step_000_cont_0_storage.tz --rpc https://hangzhounet.smartpy.io
+SmartPy.sh originate-contract --code ./build/PiXLTez/step_000_cont_0_contract.tz --storage ./build/PiXLTez/step_000_cont_0_storage.tz --rpc https://hangzhounet.smartpy.io
